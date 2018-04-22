768664236ac02c76b4f35430e108d4c884a8f34a
Oğuz Kırçiçek <2013010206098@ogrenci.karabuk.edu.tr>
2018-04-22 23:32:33 +0300

    Update kafkaproducer.py

--- Kafka+SparkStreaming/kafkaproducer.py
+++ Kafka+SparkStreaming/kafkaproducer.py
@@ -10,10 +10,10 @@ kafka=KafkaClient('localhost:9092')
 producer=SimpleProducer(kafka)
 
 
-consumer_key="dx554RgT5Jl5JFIiN82qfYuhh"
-consumer_secret="1To1V0iY8sFwUgWQBjKJ02UNLcL0k1Kq6ApCliA975R4J4LB9M"
-access_token="347852725-eqNvM2p9wqnvVJwYVzSyQOEKl2X0gBYntYFy0os4"
-access_token_secret="YVqrjLDf3nI5v41vYbXE3Wq8ulWrQyG8V4QRib9x4dfF3"
+consumer_key="*************"
+consumer_secret="******************"
+access_token="**************"
+access_token_secret="********************"
 
 baglanti=OAuthHandler(consumer_key, consumer_secret)
 baglanti.set_access_token(access_token,access_token_secret)
